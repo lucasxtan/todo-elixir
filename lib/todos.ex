15a27a81bdee7942a2b03779fc70af94775c67c8
Lucas Tan <sheng.x.tan@gmail.com>
2022-07-04 22:07:42 -0600

    refactored variable with pipe operator

--- lib/todos.ex
+++ lib/todos.ex
@@ -1,7 +1,13 @@
 defmodule Todos do
   def create_todos do
-    number_of_tasks = IO.gets("Enter the number of tasks you want to add: ")
-    {number_of_tasks, _q} = Integer.parse(number_of_tasks)
+    # old code
+    # number_of_tasks = IO.gets("Enter the number of tasks you want to add: ") # {3, \n}
+    # {number_of_tasks, _q} = Integer.parse(number_of_tasks)
+    #the _ in front of q makes elixir know we're not looking at it or using it, otherwise elixir complains we're not using it
+    # number_of_tasks
+    # new code - pipe operator |>
+    # The pipe operator passes the result of an expression as the first parameter of another expression
+    {number_of_tasks, _} = IO.gets("Enter the number of tasks you want to add: ") |> Integer.parse()
     number_of_tasks
   end
 end
