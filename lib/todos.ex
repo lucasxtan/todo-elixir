4879b5f8b5cab2dcdc11d76831839faaff5da953
Lucas Tan <sheng.x.tan@gmail.com>
2022-07-04 21:48:20 -0600

    create variables

--- lib/todos.ex
+++ lib/todos.ex
@@ -1,3 +1,7 @@
 defmodule Todos do
-
+  def create_todos do
+    number_of_tasks = IO.gets("Enter the number of tasks you want to add: ")
+    {number_of_tasks, _q} = Integer.parse(number_of_tasks)
+    number_of_tasks
+  end
 end
